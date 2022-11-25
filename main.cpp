e497df3250e148bf1e0069d9ad6e39d2199cebab
qbibubi <jakub.sobieraj2137@gmail.com>
2022-11-25 21:25:25 +0100

    Not fizzbuzz but greater/smaller

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
-template<std::size_t size>
-struct fizz_struct_T {
-  static constexpr auto get_replacement_word() {
-    if constexpr (number % 3==0 && number != 0) {
-      return StrLit("Fizz");
-    } else {
-      return StrLit("");
-    }
-  }
+#include <iostream>
+
+template<size_t arg1, size_t arg2>
+struct meta_compare{
+  static constexpr auto smallerthan()
+  {
+    if constexpr(arg1 < arg2) return true;
+    return false;
+  };
+  static constexpr auto greaterthan()
+  {
+    if constexpr(arg1 > arg2) return true;
+    return false;
+  };
+};
+
+int main()
+{
+  auto constexpr sml = meta_compare<5,3>::smallerthan();
+  auto constexpr big = meta_compare<7,1>::greaterthan();
+
+  std::cout << sml << " " << big << " oh my god\n";
+  return 0;
 };
